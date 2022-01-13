feb6fb087b607ef245e57342db64bd8340cd2223
camil <cll.camil@hotmail.com>
2022-01-13 11:10:44 +0100

    vehicules

--- MDD/init.sql
+++ MDD/init.sql
@@ -103,6 +103,7 @@ CREATE TABLE Capteur(
 CREATE TABLE Vehicule(
 	"id" int not null,
 	"type" varchar(25) not null,
+	"position" json not null,
 	"idcaserne" int not null,
 	"disponible" boolean not null,
 	PRIMARY KEY (id),
@@ -118,3 +119,53 @@ CREATE TABLE Intervention(
 	"etat" int not null,
 	PRIMARY KEY (id)
 );
+
+INSERT INTO caserne (id, position, "listeVehicule") VALUES (1,'{"x":15,"y":25}','[{"id":1,"type":"Camion","position":{"x":15,"y":25},"idcaserne":1,"disponible":true},{"id":2,"type":"Camion","position":{"x":15,"y":25},"idcaserne":1,"disponible":true},{"id":3,"type":"Camion","position":{"x":15,"y":25},"idcaserne":1,"disponible":true},{"id":4,"type":"Camion","position":{"x":15,"y":25},"idcaserne":1,"disponible":true},{"id":5,"type":"Camion","position":{"x":15,"y":25},"idcaserne":1,"disponible":true},{"id":21,"type":"Voiture","position":{"x":15,"y":25},"idcaserne":1,"disponible":true},{"id":22,"type":"Voiture","position":{"x":15,"y":25},"idcaserne":1,"disponible":true},{"id":23,"type":"Voiture","position":{"x":15,"y":25},"idcaserne":1,"disponible":true}]');
+INSERT INTO caserne (id, position, "listeVehicule") VALUES (2,'{"x":45,"y":25}','[{"id":6,"type":"Camion","position":{"x":45,"y":25},"idcaserne":2,"disponible":true},{"id":7,"type":"Camion","position":{"x":45,"y":25},"idcaserne":2,"disponible":true},{"id":8,"type":"Camion","position":{"x":45,"y":25},"idcaserne":2,"disponible":true},{"id":9,"type":"Camion","position":{"x":45,"y":25},"idcaserne":2,"disponible":true},{"id":10,"type":"Camion","position":{"x":45,"y":25},"idcaserne":2,"disponible":true},{"id":24,"type":"Voiture","position":{"x":45,"y":25},"idcaserne":2,"disponible":true},{"id":25,"type":"Voiture","position":{"x":45,"y":25},"idcaserne":2,"disponible":true},{"id":26,"type":"Voiture","position":{"x":45,"y":25},"idcaserne":2,"disponible":true}]');
+INSERT INTO caserne (id, position, "listeVehicule") VALUES (3,'{"x":45,"y":75}', '[{"id":11,"type":"Camion","position":{"x":45,"y":75},"idcaserne":3,"disponible":true},{"id":12,"type":"Camion","position":{"x":45,"y":75},"idcaserne":3,"disponible":true},{"id":13,"type":"Camion","position":{"x":45,"y":75},"idcaserne":3,"disponible":true},{"id":14,"type":"Camion","position":{"x":45,"y":75},"idcaserne":3,"disponible":true},{"id":15,"type":"Camion","position":{"x":45,"y":75},"idcaserne":3,"disponible":true},{"id":27,"type":"Voiture","position":{"x":45,"y":75},"idcaserne":3,"disponible":true},{"id":28,"type":"Voiture","position":{"x":45,"y":75},"idcaserne":3,"disponible":true},{"id":29,"type":"Voiture","position":{"x":45,"y":75},"idcaserne":3,"disponible":true}]');
+INSERT INTO caserne (id, position, "listeVehicule") VALUES (4,'{"x":15,"y":75}', '[{"id":16,"type":"Camion","position":{"x":15,"y":75},"idcaserne":4,"disponible":true},{"id":17,"type":"Camion","position":{"x":15,"y":75},"idcaserne":4,"disponible":true},{"id":18,"type":"Camion","position":{"x":15,"y":75},"idcaserne":4,"disponible":true},{"id":19,"type":"Camion","position":{"x":15,"y":75},"idcaserne":4,"disponible":true},{"id":20,"type":"Camion","position":{"x":15,"y":75},"idcaserne":4,"disponible":true},{"id":30,"type":"Voiture","position":{"x":15,"y":75},"idcaserne":4,"disponible":true},{"id":31,"type":"Voiture","position":{"x":15,"y":75},"idcaserne":4,"disponible":true},{"id":32,"type":"Voiture","position":{"x":15,"y":75},"idcaserne":4,"disponible":true}]');
+
+
+
+INSERT INTO vehicule (id, type, position, idcaserne, disponible) VALUES (1, 'Camion', '{"x":15,"y":25}', 1, true);
+INSERT INTO vehicule (id, type, position, idcaserne, disponible) VALUES (2, 'Camion', '{"x":15,"y":25}', 1, true);
+INSERT INTO vehicule (id, type, position, idcaserne, disponible) VALUES (3, 'Camion', '{"x":15,"y":25}', 1, true);
+INSERT INTO vehicule (id, type, position, idcaserne, disponible) VALUES (4, 'Camion', '{"x":15,"y":25}', 1, true);
+INSERT INTO vehicule (id, type, position, idcaserne, disponible) VALUES (5, 'Camion', '{"x":15,"y":25}', 1, true);
+
+INSERT INTO vehicule (id, type, position, idcaserne, disponible) VALUES (6, 'Camion', '{"x":45,"y":25}', 2, true);
+INSERT INTO vehicule (id, type, position, idcaserne, disponible) VALUES (7, 'Camion', '{"x":45,"y":25}', 2, true);
+INSERT INTO vehicule (id, type, position, idcaserne, disponible) VALUES (8, 'Camion', '{"x":45,"y":25}', 2, true);
+INSERT INTO vehicule (id, type, position, idcaserne, disponible) VALUES (9, 'Camion', '{"x":45,"y":25}', 2, true);
+INSERT INTO vehicule (id, type, position, idcaserne, disponible) VALUES (10, 'Camion', '{"x":45,"y":25}', 2, true);
+
+INSERT INTO vehicule (id, type, position, idcaserne, disponible) VALUES (11, 'Camion', '{"x":45,"y":75}', 3, true);
+INSERT INTO vehicule (id, type, position, idcaserne, disponible) VALUES (12, 'Camion', '{"x":45,"y":75}', 3, true);
+INSERT INTO vehicule (id, type, position, idcaserne, disponible) VALUES (13, 'Camion', '{"x":45,"y":75}', 3, true);
+INSERT INTO vehicule (id, type, position, idcaserne, disponible) VALUES (14, 'Camion', '{"x":45,"y":75}', 3, true);
+INSERT INTO vehicule (id, type, position, idcaserne, disponible) VALUES (15, 'Camion', '{"x":45,"y":75}', 3, true);
+
+INSERT INTO vehicule (id, type, position, idcaserne, disponible) VALUES (16, 'Camion', '{"x":15,"y":75}', 4, true);
+INSERT INTO vehicule (id, type, position, idcaserne, disponible) VALUES (17, 'Camion', '{"x":15,"y":75}', 4, true);
+INSERT INTO vehicule (id, type, position, idcaserne, disponible) VALUES (18, 'Camion', '{"x":15,"y":75}', 4, true);
+INSERT INTO vehicule (id, type, position, idcaserne, disponible) VALUES (19, 'Camion', '{"x":15,"y":75}', 4, true);
+INSERT INTO vehicule (id, type, position, idcaserne, disponible) VALUES (20, 'Camion', '{"x":15,"y":75}', 4, true);
+
+
+
+
+INSERT INTO vehicule (id, type, position, idcaserne, disponible) VALUES (21, 'Voiture', '{"x":15,"y":25}', 1, true);
+INSERT INTO vehicule (id, type, position, idcaserne, disponible) VALUES (22, 'Voiture', '{"x":15,"y":25}', 1, true);
+INSERT INTO vehicule (id, type, position, idcaserne, disponible) VALUES (23, 'Voiture', '{"x":15,"y":25}', 1, true);
+
+INSERT INTO vehicule (id, type, position, idcaserne, disponible) VALUES (24, 'Voiture', '{"x":45,"y":25}', 2, true);
+INSERT INTO vehicule (id, type, position, idcaserne, disponible) VALUES (25, 'Voiture', '{"x":45,"y":25}', 2, true);
+INSERT INTO vehicule (id, type, position, idcaserne, disponible) VALUES (26, 'Voiture', '{"x":45,"y":25}', 2, true);
+
+INSERT INTO vehicule (id, type, position, idcaserne, disponible) VALUES (27, 'Voiture', '{"x":45,"y":75}', 3, true);
+INSERT INTO vehicule (id, type, position, idcaserne, disponible) VALUES (28, 'Voiture', '{"x":45,"y":75}', 3, true);
+INSERT INTO vehicule (id, type, position, idcaserne, disponible) VALUES (29, 'Voiture', '{"x":45,"y":75}', 3, true);
+
+INSERT INTO vehicule (id, type, position, idcaserne, disponible) VALUES (30, 'Voiture', '{"x":15,"y":75}', 4, true);
+INSERT INTO vehicule (id, type, position, idcaserne, disponible) VALUES (31, 'Voiture', '{"x":15,"y":75}', 4, true);
+INSERT INTO vehicule (id, type, position, idcaserne, disponible) VALUES (32, 'Voiture', '{"x":15,"y":75}', 4, true);
