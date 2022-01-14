8bef8315805e65f3cd55dad45addbb3cca3dd893
HADJEB SAMI p1804898 <sami.hadjeb@etu.univ-lyon1.fr>
2022-01-14 02:16:00 +0100

    test

--- SimulationJava/src/simulationjava/controller/Controller.java
+++ SimulationJava/src/simulationjava/controller/Controller.java
@@ -51,7 +51,7 @@ public class Controller {
     public static void GenereFeu(Capteur[] tabCapteur){
         int x = new Random().nextInt(101);
         int y = new Random().nextInt(61);
-        Coord position = new Coord(30, 30);
+        Coord position = new Coord(15, 15);
         
         int intensite = new Random().nextInt(9);
         if (intensite == 0){
